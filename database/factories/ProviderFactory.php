f386923dd8a0cfae6039e722e5380a342649f9c5
Andrey Abramov <karaulshickov@yandex.ru>
2022-11-30 11:26:01 +0300

    Добавил модель провайдера

---
+++ database/factories/ProviderFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Provider;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends Factory<Provider>
+ */
+class ProviderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "title" => fake()->title,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Provider;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -19,5 +20,7 @@ class DatabaseSeeder extends Seeder
                 "email" => "test@example.com",
             ]);
         }
+
+        Provider::factory()->create();
     }
 }
